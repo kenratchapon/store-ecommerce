487d3ccc8d8fbe6bcdb14ae70ffa4c31e0acb9d8
Ken Ratchapon <ratchapon.pk@outlook.com>
2023-07-30 16:04:09 +0700

    1.create homepage 2.create footer 3.create Navbar -navbar -main-nave(create lib folder to import cn function/ create types.ts interface of Billboard,Category) 4.env file -make sure gitIgnore .env -add NEXT_PUBLIC_API_URL={api} 5.create folder actions 6.create product card

---
+++ actions/get-products.tsx
@@ -0,0 +1,34 @@
+import { Category, Product } from "@/types";
+
+//npm i query-string
+import qs from "query-string"
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`
+
+interface Query{
+  categoryId?: string
+  colorId?: string
+  sizeId?: string
+  isFeatured?: boolean
+}
+
+const getProducts = async(query: Query): Promise<Product[]> => {
+
+    const url = qs.stringifyUrl({
+      url: URL,
+      query:{
+        colorId: query.colorId,
+        sizeId: query.sizeId,
+        categoryId: query.categoryId,
+        isFeatured: query.isFeatured
+      }
+    })
+
+    const res = await fetch(URL)
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')
+      }
+    return res.json()
+}
+
+export default getProducts
